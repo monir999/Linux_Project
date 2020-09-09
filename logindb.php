743e8c6a17986dd9253b8a93dbc17c026a55a5f7
md.moniruzzaman <57668333+monir999@users.noreply.github.com>
2020-09-09 23:33:04 +0200

    Update logindb.php

--- logindb.php
+++ logindb.php
@@ -8,10 +8,10 @@ $conn = mysql_connect("$host", "$username", "$password")or die("can not connect"
 mysql_select_db("db_name")or die("can not select db");
 $myusername = $_POST['usr'];
 $mypassword = $_POST['pwd'];
-$myusername = stripslashes($myusername);
-$mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
+#$myusername = stripslashes($myusername);
+#$mypassword = stripslashes($mypassword);
+#$myusername = mysql_real_escape_string($myusername);
+#$mypassword = mysql_real_escape_string($mypassword);
 
 $sql="select * from $tbl_name where password='$mypassword' AND name='$myusername'";
 
